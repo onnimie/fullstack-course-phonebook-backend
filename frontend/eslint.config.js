626a480c59cd4cb978f8d4a0e10b493963ab96c8
onnimiettinen <onnimie@gmail.com>
2024-08-02 22:27:25 +0300

    turn off some react rules, and fix lint stuff in eslint-config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,19 +1,19 @@
-import globals from "globals";
+import globals from 'globals'
 import stylisticJs from '@stylistic/eslint-plugin-js'
 import js from '@eslint/js'
-import pluginReact from "eslint-plugin-react";
+import pluginReact from 'eslint-plugin-react'
 
 export default [
-  //js.configs.recommended,
+  js.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
+    files: ['**/*.{js,mjs,cjs,jsx}'],
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
       },
-      ecmaVersion: "latest",
+      ecmaVersion: 'latest',
     },
     plugins: {
       '@stylistic/js': stylisticJs
@@ -51,9 +51,11 @@ export default [
           'argsIgnorePattern': '__'
         }
       ],
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
     },
   },
-  { 
-    ignores: ["dist/**", "build/**"],
+  {
+    ignores: ['dist/**', 'build/**'],
   },
 ]
