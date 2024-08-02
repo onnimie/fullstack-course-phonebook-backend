858641db475a605aa9d72f35674b0e6f9894e941
onnimiettinen <onnimie@gmail.com>
2024-08-02 22:17:02 +0300

    add esilnt to frontend

---
+++ frontend/eslint.config.js
@@ -0,0 +1,59 @@
+import globals from "globals";
+import stylisticJs from '@stylistic/eslint-plugin-js'
+import js from '@eslint/js'
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  //js.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
+    },
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'windows'
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      '@stylistic/js/semi': [
+        'error',
+        'never'
+      ],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+      'arrow-spacing': [
+        'error', { 'before': true, 'after': true },
+      ],
+      'no-console': 'off',
+      'no-unused-vars': [
+        'error',
+        {
+          //'varsIgnorePattern': '__',
+          'argsIgnorePattern': '__'
+        }
+      ],
+    },
+  },
+  { 
+    ignores: ["dist/**", "build/**"],
+  },
+]
